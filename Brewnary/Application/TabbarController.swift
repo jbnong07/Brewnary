7558f2584628c6ef1df43ebb2e15f298d1eac1c2
jbnong07 <jbnong07@naver.com>
2025-04-22 16:13:17 +0900

    ✨ [Feature] Add data models

---
+++ Brewnary/Application/TabbarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brewnary
+//
+//  Created by 박진홍 on 4/14/25.
+//
+
+import UIKit
+
+class TabbarController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
