4dd1c31ebecb68f2b4edd8d96d3298f7191688ba
jbnong07 <jbnong07@naver.com>
2025-04-28 21:00:56 +0900

    ♻️ [Refactor] Core data repository context

---
+++ Brewnary/Brewnary/Data/Mapper/BeanMapper.swift
@@ -0,0 +1,15 @@
+//
+//  BeanMapper.swift
+//  Brewnary
+//
+//  Created by 박진홍 on 4/28/25.
+//
+
+import Foundation
+import CoreData
+
+struct BeanMapper {
+    static func map(from entity: BeanEntity) throws -> Bean {
+        
+    }
+}
