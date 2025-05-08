1bc556b76be6df71194e40a0d5354b93df46573d
jbnong07 <jbnong07@naver.com>
2025-05-08 11:21:55 +0900

    ✨ [Feature] Bean list row view

---
+++ Brewnary/Brewnary/Feature/Bean/View/BeanListRowView.swift
@@ -0,0 +1,50 @@
+//
+//  BeanRowView.swift
+//  Brewnary
+//
+//  Created by 박진홍 on 4/25/25.
+//
+
+import SwiftUI
+
+struct BeanListRowView: View {
+    private let name: String
+    private let roaster: String
+    private let date: Date
+    
+    init(name: String, roaster: String, date: Date) {
+        self.name = name
+        self.roaster = roaster
+        self.date = date
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(name)
+                .bold()
+                .font(.system(size: UIMetric.FontSize.subTitle))
+            Text(roaster)
+                .font(.system(size: UIMetric.FontSize.body))
+            Text(date.toIso8601Date())
+                .font(.system(size: UIMetric.FontSize.caption))
+            
+        }
+        .padding(.horizontal, UIMetric.Padding.regular)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical,UIMetric.Padding.regular)
+        .background {
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundStyle(.gray)
+        }
+    }
+    
+}
+
+#if DEBUG
+struct BeanListPreview: PreviewProvider {
+    static var previews: some View {
+        BeanListRowView(name: "hello", roaster: "world", date: Date.now)
+    }
+}
+#endif
+
